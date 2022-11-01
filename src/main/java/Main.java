81dde7472a93c8543032ff019c1c99e1ddae8749
AariyJP <aariyjp@aariy.net>
2022-11-01 22:37:21 +0900

    ScheduleBot

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -14,6 +14,7 @@ import java.util.TimerTask;
 public class Main extends ListenerAdapter
 {
     public static void main(String[] args) {
+        System.out.println("ScheduleBot is starting up");
         System.out.println("(c) 2022 Aariy.NET");
         JDABuilder jda = JDABuilder.createDefault(System.getProperty("token"));
         jda
